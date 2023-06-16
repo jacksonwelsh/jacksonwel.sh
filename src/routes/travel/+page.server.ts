7d8f6ab6e3f35326ef52ff34c247f3f2a612e010
Jackson Welsh <me@jacksonwel.sh>
2023-06-16 14:48:49 -0500

    add caching to travel

--- src/routes/travel/+page.server.ts
+++ src/routes/travel/+page.server.ts
@@ -1,8 +1,12 @@
 import type { ServerLoad } from '@sveltejs/kit';
 import { getAllPosts } from './write/workers';
 
-export const load: ServerLoad = async () => {
+export const load: ServerLoad = async ({ setHeaders }) => {
 	const posts = await getAllPosts();
+
+	setHeaders({
+		'cache-control': 'max-age=600'
+	});
 
 	return { posts };
 };
