70b61dbc03691c181033869a68f8f5d207d96c3b
Jackson Welsh <me@jacksonwel.sh>
2024-01-16 21:32:17 -0800

    add breadcrumbs to /blog

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,4 +1,10 @@
-<div class="container mx-auto" />
+<div class="container mx-auto">
+	<div class="text-left mt-3 text-slate-400 print:hidden flex">
+		<a href="/" class="text-blue-400 hover:underline">~</a>
+		/blog
+	</div>
+</div>
+
 <section class="container mx-auto flex h-screen flex-wrap content-center relative">
 	<h1 class="font-bold text-6xl w-full">blog</h1>
 	<ul class="my-4">
