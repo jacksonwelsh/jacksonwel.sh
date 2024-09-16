f9f580a6a6c942439c2319726edd0ddbd6cb83fc
Jackson Welsh <me@jacksonwel.sh>
2024-09-15 20:08:14 -0700

    janky half-solution to safari focus bug

--- src/lib/input.svelte
+++ src/lib/input.svelte
@@ -14,7 +14,9 @@
 	export let required = false;
 	export let className = '';
 	export let placeholder = '';
+	export let inputmode: 'text' | 'numeric' | 'decimal' | null = null;
 	export let disabled = false;
+	export let pattern = '';
 
 	const dispatch = createEventDispatcher();
 	const onFocus = () => dispatch('focus');
@@ -27,7 +29,14 @@
 	let inputElement: HTMLInputElement;
 
 	export const focus = () => {
-		setTimeout(() => inputElement?.focus());
+		setTimeout(() => {
+			inputElement?.focus();
+		});
+
+		if (inputElement == document.activeElement) {
+			return;
+		}
+		inputElement?.focus();
 	};
 </script>
 
@@ -46,6 +55,8 @@
 			{value}
 			{placeholder}
 			{disabled}
+			{inputmode}
+			{pattern}
 			class={[
 				'w-full h-10 font-mono rounded-md p-2 focus:ring-teal-500 focus:ring-2 focus:outline-none border disabled:opacity-100 dark:border-slate-700 dark:bg-slate-900 dark:disabled:!bg-slate-800 disabled:bg-slate-100 disabled:cursor-not-allowed',
 				className
