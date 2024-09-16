5ce0979a8b6d91d5676df2066d8ab02c880b9d3f
Jackson Welsh <me@jacksonwel.sh>
2024-09-15 19:34:17 -0700

    use neon hints for dark mode

--- src/routes/popdle/hint.svelte
+++ src/routes/popdle/hint.svelte
@@ -4,13 +4,18 @@
 	export let state: HintState = HintState.DISABLED;
 
 	const stateStyles = {
-		[HintState.DISABLED]: 'dark:bg-slate-800 bg-slate-100 cursor-not-allowed',
-		[HintState.UNKNOWN]: 'bg-white dark:bg-slate-900 cursor-text',
-		[HintState.VERY_HIGH]: 'bg-red-500',
-		[HintState.HIGH]: 'bg-orange-500',
-		[HintState.CORRECT]: 'bg-green-500',
-		[HintState.LOW]: 'bg-blue-500',
-		[HintState.VERY_LOW]: 'bg-indigo-500'
+		[HintState.DISABLED]: 'dark:bg-slate-800 bg-slate-100 cursor-not-allowed dark:border-gray-700',
+		[HintState.UNKNOWN]: 'bg-white dark:bg-slate-900 cursor-text dark:border-gray-700',
+		[HintState.VERY_HIGH]:
+			'bg-red-500 dark:bg-red-900/50 dark:border-red-500 dark:text-red-500 dark:border-l',
+		[HintState.HIGH]:
+			'bg-orange-500 dark:bg-orange-900/50 dark:border-orange-500 dark:text-orange-500 dark:border-l',
+		[HintState.CORRECT]:
+			'bg-green-500 dark:bg-green-900/50 dark:border-green-500 dark:text-green-500 dark:border-l',
+		[HintState.LOW]:
+			'bg-blue-500 dark:bg-blue-900/50 dark:border-blue-500 dark:text-blue-500 dark:border-l',
+		[HintState.VERY_LOW]:
+			'bg-indigo-500 dark:bg-indigo-900/50 dark:border-indigo-500 dark:text-indigo-500 dark:border-l'
 	};
 
 	const stateContents = {
@@ -28,7 +33,7 @@
 <div
 	on:mousedown
 	class={[
-		'border group-focus-within:border-2 group-focus-within:border-l-0 border-l-0 border-gray-200 dark:border-gray-700 group-focus-within:border-teal-500 rounded-r-md w-24 font-mono font-mono-bold flex items-center justify-center text-xl h-full',
+		'border group-focus-within:border-2 group-focus-within:border-l-0 border-l-0 border-gray-200 group-focus-within:border-teal-500 rounded-r-md w-24 font-mono font-mono-bold flex items-center justify-center text-xl h-full',
 		stateStyles[state]
 	].join(' ')}
 >
